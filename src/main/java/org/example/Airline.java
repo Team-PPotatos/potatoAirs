18381452322320f287cae9e2e41e4b6c6e6efc16
SO-HUII <haejuuun925@gmail.com>
2023-05-15 16:22:27 +0900

    [feat] Airline 클래스의 필요 메소드 초기 설정

--- src/main/java/org/example/Airline.java
+++ src/main/java/org/example/Airline.java
@@ -1,4 +1,18 @@
 package org.example;
 
 public class Airline {
+    public void longin() {
+    }
+
+    public void ticketReserve() {
+    }
+
+    public void ticketCancel() {
+    }
+
+    public void reserveListShow() {
+    }
+
+    public void changePassword() {
+    }
 }
