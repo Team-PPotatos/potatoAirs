5234a5e2ef44f82b47c65cbac83b029e09817f02
SO-HUII <haejuuun925@gmail.com>
2023-05-15 16:31:11 +0900

    Revert "[feat] Airline 클래스의 필요 메소드 초기 설정"
    This reverts commit 18381452322320f287cae9e2e41e4b6c6e6efc16.

--- src/main/java/org/example/Airline.java
+++ src/main/java/org/example/Airline.java
@@ -1,18 +1,4 @@
 package org.example;
 
 public class Airline {
-    public void longin() {
-    }
-
-    public void ticketReserve() {
-    }
-
-    public void ticketCancel() {
-    }
-
-    public void reserveListShow() {
-    }
-
-    public void changePassword() {
-    }
 }
