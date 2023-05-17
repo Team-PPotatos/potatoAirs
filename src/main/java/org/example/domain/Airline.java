9d921c5b4c4a3ce426ffba5a4700ca4808ecc16f
hanna0527 <114378724+hanna0527@users.noreply.github.com>
2023-05-17 21:19:08 +0900

    [Revert] Airline
    revert Airline

--- src/main/java/org/example/domain/Airline.java
+++ src/main/java/org/example/domain/Airline.java
@@ -36,8 +36,8 @@ public class Airline{
         return seats;
     }
 
-    public void setSeats(int planeNum, int seatNum, int reserve){   // 항공편, 좌석 번호를 받아 1 또는 0 set
-        this.seats[planeNum][seatNum] = reserve;
+    public void setSeats(int[][] seats){
+        this.seats = seats;
     }
     public String[][] getPlanes(){
         return planes;
@@ -55,7 +55,9 @@ public class Airline{
         this.planeNum = planeNum;
     }
 
-    public int getSeatNum() { return seatNum; }
+    public int getSeatNum() {
+        return seatNum;
+    }
 
     public void setSeatNum(int seatNum) {
         this.seatNum = seatNum;
