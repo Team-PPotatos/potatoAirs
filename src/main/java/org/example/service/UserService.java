b8b3b5f0a8a42c9a66468b8b7eb28acf6522b311
DESKTOP-KTEEVGU\hanme <2hanme527@naver.com>
2023-05-21 21:31:58 +0900

    [feat] ArrayList를 사용한 수정
    ArrayList를 이용한 전체적인 수정
    인덱스로 접근을 하면 삭제했을 때 자꾸 지운 인덱스까지 참조해서 np exception이 떠서 수정해봤습니다.

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,46 +4,59 @@ import org.example.domain.Airline;
 import org.example.domain.User;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserService {
-    User[] userInfo = new User[20];
-    Airline[] flightInfo = new Airline[20];
-    Airline airline = new Airline();
-
-
-    User user = new User();
     Scanner sc = new Scanner(System.in);
+//    User[] userInfo = new User[20];
+    ArrayList<User> userInfo = new ArrayList<>();               // 모든 회원 정보
 
-    int idx = 0;
-    int flight_idx = 0;
+//    Airline[] flightInfo = new Airline[20];
 
-    int LoginCorrectPersonNum;     // isMember 에서 찾은 사람의 번호(i) 저장
+    ArrayList<Airline> flightInfo = new ArrayList<>();          // 모든 예약 정보
 
-    ArrayList<Integer> canceledNum = new ArrayList<>(); // 예약 삭제 번호 리스트 (NULL POINTER 참조 막는 용)
+
+    Airline airline = new Airline();    // 항공사 DB 확인용
+
+
+    User loginUser = new User();
+
+    /* iterator 를 사용하여 순회하기 때문에 index 가 없어도 된다. */
+    // int idx = 0;
+    // int flight_idx = 0;
+
+    /* isMember 에서 찾은 사람의 번호(i) 저장 (객체 저장 방식으로 변경했기 때문에 필요 X)*/
+    // int LoginCorrectPersonNum;
+
+    /* 예약 삭제 번호 리스트 (index 로 순회하지 않기 때문에 nullPointerException 발생도 없다) */
+    //    ArrayList<Integer> canceledNum = new ArrayList<>();
 
     public boolean isMember(String id, String pwd) {    // 로그인 할 때
         boolean check = false;                          // 입력한 로그인 정보가 회원 DB에 존재하는가?
-        for (int i = 0; i < idx; i++) {
+        Iterator<User> userIterator = userInfo.iterator();          // userInfo 순회
+
+        while (userIterator.hasNext()) {
             // System.out.println(id + pwd);
-            if (id.equals(userInfo[i].getId()) && pwd.equals(userInfo[i].getPwd())) {
-                LoginCorrectPersonNum = i;
-                check = true;                           // 로그인 정보를 찾음
+            User user = userIterator.next();    // 확인할 User 객체
+            if (id.equals(user.getId()) && pwd.equals(user.getPwd())) {
+                loginUser = user;       // 로그인 정보를 찾음
+                check = true;
                 break;
             }
         }
         return check;
     }
 
-    public int getIdx(String pwd) {
-        for (int i = 0; i < idx; i++) {
-            if (pwd.equals(userInfo[i].getPwd())) {
-                return i;
-            }
-        }
-        return 31;
-    }
+//    public int getIdx(String pwd) {               // id를 활용
+//        while (userIterator.hasNext()) {
+//            if (pwd.equals(userInfo[i].getPwd())) {
+//                return i;
+//            }
+//        }
+//        return 31;
+//    }
 
     public boolean containsBlank(String str) {   // 공백 포함 시 false, 없다면 true
         if (str.isEmpty() || str.contains(" ")) {               // 같은 아이디일 시
@@ -53,11 +66,11 @@ public class UserService {
         return false;
     }
 
-    public boolean exsitingId(String id) {          // 입력한 id 정보가 회원 DB에 이미 존재하는가?
-        for (int i = 0; i < idx; i++) {
+    public boolean exsistingId(String id) {          // 입력한 id 정보가 회원 DB에 이미 존재하는가?
+        Iterator<User> userIterator = userInfo.iterator();          // userInfo 순회
+        while (userIterator.hasNext()) {
             // System.out.println(id + pwd);
-            if (id.equals(userInfo[i].getId())) {   // 로그인 정보를 찾음
-                System.out.println("id가 이미 존재합니다!");
+            if (id.equals(userIterator.next().getId())) {   // 같은 id를 찾음
                 return true;
             }
         }
@@ -77,7 +90,12 @@ public class UserService {
             id = sc.nextLine();
 
             /* 공백 또는 id가 이미 존재 */
-            if (containsBlank(id) || exsitingId(id)) continue;
+            if (exsistingId(id)){
+                System.out.println("id가 이미 존재합니다!");
+                continue;
+            }
+
+            if (containsBlank(id)) continue;
 
             System.out.print("비밀번호를 입력해주세요. : ");
             pwd = sc.nextLine();
@@ -90,55 +108,50 @@ public class UserService {
             break;
         }
 
-        userInfo[idx] = new User(id, pwd, name, 500000);
-        idx++;
+        userInfo.add(new User(id, pwd, name, 500000));
         // System.out.println(idx);
         System.out.println("회원 가입을 완료 했습니다.");
     }
 
     public void login() {
-        if (userInfo[0] == null) {  // 회원 정보 존재 X
+        if (userInfo.isEmpty()) {  // 회원 정보 존재 X
             System.out.println("회원가입을 먼저 진행해주세요.");
             return;
         }
 
-        if (userInfo[LoginCorrectPersonNum].isLogin()) {        // 이미 로그인 되어있는 상태
-            System.out.println("이미 로그인 상태입니다.");
-            return;
-        }
-
-        System.out.print("아이디를 입력해주세요 : ");
-        String id = sc.nextLine();
-
-        System.out.print("비밀번호를 입력해주세요 : ");
-        String pwd = sc.nextLine();
-
-        while (!isMember(id, pwd)){                                  // 일치하지 않음
-            System.out.println("로그인에 실패했습니다. 아이디와 비밀번호가 일치하지 않습니다.");
+        while (true) {                                  // 일치하지 않음
             System.out.print("아이디를 입력해주세요 : ");
-            id = sc.nextLine();
+            String id = sc.nextLine();
 
             System.out.print("비밀번호를 입력해주세요 : ");
-            pwd = sc.nextLine();
-        }
+            String pwd = sc.nextLine();
 
-        userInfo[LoginCorrectPersonNum].setLogin(true); // while문을 빠져나왔다 == 로그인 정보가 있다.
-        System.out.println("로그인에 성공했습니다.");
+            // 작성된 계정정보의 islogin이 true라면, 이미 로그인되어있습니다 & break 추가 예정
+            String userId = id;     // id를 isMember가 잘 읽지 못함
+            String userPwd = pwd;   // 잘 읽게 해주는 다리 역할을 해주는 변수
+
+            if (!isMember(userId, userPwd)) {
+                System.out.println("로그인에 실패했습니다. 아이디와 비밀번호가 일치하지 않습니다.");
+                break;
+            } else {    // 해당 객체를 loginUser에 저장
+                loginUser.setLogin(true);
+                System.out.println("로그인에 성공했습니다.");
+                return;
+            }
+        }
     }
 
     public void showFlight() {
-        if (userInfo[0] == null) {  // 회원 정보 존재 X
+        if (userInfo.isEmpty()) {  // 회원 정보 존재 X
             System.out.println("회원가입을 먼저 진행해주세요.");
             return;
         }
 
-
-        // 로그인 여부 검사
-        if (!userInfo[LoginCorrectPersonNum].isLogin()) {
+        /* 아예 한 번도 로그인을 하지 않거나 로그아웃 시 */
+        if (loginUser == null || !loginUser.isLogin()) {
             System.out.println("로그인이 필요한 기능입니다!");
             return;
         }
-
 
             System.out.println();
             System.out.println("====================== 항공편 목록 ====================== \n");
@@ -166,15 +179,14 @@ public class UserService {
                 return;
             }
 
+        String userId = loginUser.getId();
 
-            String userId = userInfo[LoginCorrectPersonNum].getId();
-            airline.setSeats(planeNum - 1, seatNum - 1, 0);     // 예약해서 0으로 바꾸는 함수
+        /* 예약해서 0으로 바꾸는 함수 */
+        airline.setSeats(planeNum - 1, seatNum - 1, 0);
 
-            /* 회원 가입때의 id를 중복 불가능으로 만들었기 때문에 id로 flightInfo를 조회할 수 있다. */
-            flightInfo[flight_idx] = new Airline(planeNum, seatNum, userId);
-            flight_idx++;
-            System.out.println(userId + " 회원님의 " + planeNum + "번째 비행기의 " + seatNum + "번째 좌석 예약이 완료되었습니다.");
-
+        /* 회원 가입때의 id를 중복 불가능으로 만들었기 때문에 id로 flightInfo 를 조회할 수 있다. */
+        flightInfo.add(new Airline(planeNum, seatNum, userId));
+        System.out.println(userId + "회원님의 " + planeNum + "번째 비행기의 " + seatNum + "번째 좌석 예약이 완료되었습니다.");
     }
 
 
@@ -183,22 +195,21 @@ public class UserService {
 
 
     public void delFlight() {
-        if (userInfo[0] == null) {  // 회원 정보 존재 X
+        if (userInfo.isEmpty()) {  // 회원 정보 존재 X
             System.out.println("회원가입을 먼저 진행해주세요.");
             return;
         }
 
-        if (!userInfo[LoginCorrectPersonNum].isLogin()) {
+        /* 아예 한 번도 로그인을 하지 않거나 로그아웃 시 */
+        if (loginUser == null || !loginUser.isLogin()) {
             System.out.println("로그인이 필요한 기능입니다!");
             return;
         }
 
-        if (flightInfo[0] == null){
-            System.out.println("예약한 내역이 없습니다.");
-            return;
-        }
-
-
+//        if (flightInfo.isEmpty()){  // 예약한 내역 자체가 X
+//            System.out.println("예약한 내역이 없습니다.");
+//            return;
+//        }                 // 중복
 
         showFlightInfo();
         System.out.print("몇 번 예약을 취소하겠습니까? : ");
@@ -212,13 +223,16 @@ public class UserService {
             System.out.println("취소 하지 않습니다.");
             return;
         }
-        canceledNum.add(cancelNum);     // 삭제 시 생기는 null 참조 방지 리스트
-        int planeNum = flightInfo[cancelNum].getPlaneNum() - 1;
-        int seatNum = flightInfo[cancelNum].getSeatNum() - 1;
 
-        System.out.println(planeNum + " " + seatNum);
+        //canceledNum.add(cancelNum);     // canceledNum 삭제
+
+        /* ArrayList를 index로 접근 */
+        int planeNum = flightInfo.get(cancelNum).getPlaneNum() - 1;
+        int seatNum = flightInfo.get(cancelNum).getSeatNum() - 1;
+
+//        System.out.println(planeNum + " " + seatNum);
         airline.setSeats(planeNum, seatNum, 1);     // 예약을 취소해서 다시 1으로 바꾸는 함수
-        flightInfo[cancelNum] = null;
+        flightInfo.remove(flightInfo.get(cancelNum));       // 예약 내역 삭제
         System.out.println("취소를 완료 했습니다.");
     }
 
@@ -227,62 +241,86 @@ public class UserService {
 
     public List<Airline> findFlightInfo(String id) {    // 여러 개일 수 있다.
         List<Airline> flightInfoArr = new ArrayList<>();
-        for (int i = 0; i < flight_idx; i++) {
+
+        Iterator<Airline> flightiterator = flightInfo.iterator(); // flightInfo 순회
+        while (flightiterator.hasNext()) {
 //            System.out.println(i);
 
-            if (canceledNum.contains(i)) {        // 삭제한 인덱스는 pass
-                continue;
-            }
-
-
-            if (id.equals(flightInfo[i].getUserId())) {    // 순회하다 해당 id로 예약한 항공 예약이 보일 시
-                flightInfoArr.add(flightInfo[i]);          // 해당 flightInfo 객체를 array에 추가
+//            if (canceledNum.contains(i)) {        // 삭제한 인덱스는 pass (NULL POINTER EXCEPTION)
+//                continue;                         // iterator 사용으로 삭제 인덱스 신경 쓸 필요 없음
+//            }
+            Airline air = flightiterator.next();
+            if (id.equals(air.getUserId())) {    // flightInfo를 순회하다 해당 id로 예약한 항공 예약이 보일 시
+                flightInfoArr.add(air);          // 해당 airline 객체를 array에 추가
             }
         }
         return flightInfoArr;
     }
 
     public void showFlightInfo() {      // 메뉴의 예매 내역 조회
-        if (flightInfo[0] == null) {
-            System.out.println("예약한 내역이 없습니다.");
-            return;
-        }
-
-        for(Airline air : findFlightInfo(userInfo[LoginCorrectPersonNum].getId())) {  // 해당 id의 예약 내역의 리스트
-            System.out.println(air.getPlaneNum() + "번 비행기의 " + air.getSeatNum() + "번 좌석");
-        }
-    }
-
-
-    public void newPassword() {
-        if (userInfo[0] == null) {  // 회원 정보 존재 X
-            System.out.println("회원가입을 먼저 진행해주세요.");
-            return;
-        }
-
-        if (!userInfo[LoginCorrectPersonNum].isLogin()) {
+        if (loginUser == null || !loginUser.isLogin()) {
             System.out.println("로그인이 필요한 기능입니다!");
             return;
         }
 
+        for(Airline air : findFlightInfo(loginUser.getId())) {  // 해당 id의 예약 내역의 리스트
+            System.out.println(air.getPlaneNum() + "번 비행기의 " + air.getSeatNum() + "번 좌석");
+        }
+    }
+
+    public User findUserFromId(String id) {          // 입력한 id 정보로 대응되는 유저를 찾는 함수
+
+        Iterator<User> userIterator = userInfo.iterator();          // userInfo 순회
+        while (userIterator.hasNext()) {
+            // System.out.println(id + pwd);
+            User foundUser = userIterator.next();
+            if (id.equals(foundUser.getId())) {        // 같은 id를 찾음
+                return foundUser;
+            }
+        }
+        return null;
+    }
+
+
+    public void newPassword() {
+        if (userInfo.isEmpty()) {  // 회원 정보 존재 X
+            System.out.println("회원가입을 먼저 진행해주세요.");
+            return;
+        }
+
+//        if (!loginUser.isLogin()) {
+//            System.out.println("로그인이 필요한 기능입니다!");
+//            return;
+//        }
+
         System.out.print("아이디를 입력해주세요. : ");
         String id = sc.nextLine();
+
+        /* id를 통해 해당 유저를 찾는다. */
+        User foundUser = findUserFromId(id);
+
+        if (foundUser == null) {     // 회원 정보에 없는 id
+            System.out.println("존재하지 않는 id입니다!");
+            return;
+        }
+        String foundPwd = foundUser.getPwd();   // id를 통해 해당 pwd 값을 가져온다.
 
         System.out.print("현재 비밀번호를 입력해주세요. : ");
         String pass = sc.nextLine();
 
-        int userNum = getIdx(pass);
-        boolean checkPw = userInfo[userNum].getPwd().equals(pass);
-        boolean checkId = userInfo[userNum].getId().equals(id);
+        // boolean checkPw = userInfo[userNum].getPwd().equals(pass);
 
-        if (!checkPw || !checkId ) {
-            System.out.println("아이디 또는 비밀번호를 잘못 입력 하셨습니다.");
-        } else {
-            System.out.print("변경할 비밀번호를 입력해주세요 : ");
-            String newPassword = sc.nextLine();
+        // int userNum = getIdx(pass);  // 비밀번호로 userinfo의 인덱스를 가져온다 (id로 가져오게 변경)
 
-            userInfo[userNum].setPwd(newPassword);
-            System.out.println("비밀번호를 " + newPassword + "로 변경했습니다.");
+        if (!foundPwd.equals(pass)) {
+            System.out.println("비밀번호를 잘못 입력 하셨습니다.");
+            return;
         }
+
+        System.out.print("변경할 비밀번호를 입력해주세요 : ");
+        String newPassword = sc.nextLine();
+
+        foundUser.setPwd(newPassword);
+        System.out.println("비밀번호를 " + newPassword + "로 변경했습니다.");
     }
 }
